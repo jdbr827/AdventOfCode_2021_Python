ddecc3eeada15d523a39959a98bd9a9d3be2ce08
Jake <reinhartjacob827@gmail.com>
2021-12-03 18:09:14 -0500

    D3: some code cleanup

--- Day3/day_3_part_2.py
+++ Day3/day_3_part_2.py
@@ -23,7 +23,7 @@ def split_data_by_bit_at_index(data: List[BitStr], idx: int) -> Tuple[List[BitSt
     return containing_zeros, containing_ones
 
 
-def compute_oxygen_generator_rating_binary(input_filename: str) -> BitStr:
+def compute_oxygen_generator_rating(input_filename: str) -> int:
     my_data: List[BitStr] = format_input(input_filename)
     n = len(my_data[0])
     idx = 0
@@ -31,29 +31,27 @@ def compute_oxygen_generator_rating_binary(input_filename: str) -> BitStr:
         (containing_zeros, containing_ones) = split_data_by_bit_at_index(my_data, idx)
         my_data = containing_ones if len(containing_ones) >= len(containing_zeros) else containing_zeros
         idx += 1
-    return my_data[0]
+    return convert_binary_to_decimal(my_data[0])
 
 
-def compute_c02_scrubber_rating_binary(input_filename: str) -> BitStr:
-    my_data: List[str] = format_input(input_filename)
+def compute_c02_scrubber_rating(input_filename: str) -> int:
+    my_data: List[BitStr] = format_input(input_filename)
     n = len(my_data[0])
     idx = 0
     while len(my_data) > 1:
         (containing_zeros, containing_ones) = split_data_by_bit_at_index(my_data, idx)
         my_data = containing_ones if len(containing_ones) < len(containing_zeros) else containing_zeros
         idx += 1
-    return my_data[0]
+    return convert_binary_to_decimal(my_data[0])
 
 
 def compute_life_support_rating(input_filename: str) -> int:
-    oxygen_generator_rating_binary: BitStr = compute_oxygen_generator_rating_binary(input_filename)
-    c02_scrubber_rating_binary: BitStr = compute_c02_scrubber_rating_binary(input_filename)
-    oxygen_generator_rating_decimal: int = convert_binary_to_decimal(oxygen_generator_rating_binary)
-    c02_scrubber_rating_decimal: int = convert_binary_to_decimal(c02_scrubber_rating_binary)
-    return oxygen_generator_rating_decimal * c02_scrubber_rating_decimal
+    oxygen_generator_rating: int = compute_oxygen_generator_rating(input_filename)
+    c02_scrubber_rating: int = compute_c02_scrubber_rating(input_filename)
+    return oxygen_generator_rating * c02_scrubber_rating
 
 
-print(compute_oxygen_generator_rating_binary('day_3_small_input.txt') == '10111')
-print(compute_c02_scrubber_rating_binary('day_3_small_input.txt') == '01010')
+print(compute_oxygen_generator_rating('day_3_small_input.txt') == 23)
+print(compute_c02_scrubber_rating('day_3_small_input.txt') == 10)
 print(compute_life_support_rating('day_3_small_input.txt') == 230)
 print(compute_life_support_rating('day_3_input.txt') == 2845944)
