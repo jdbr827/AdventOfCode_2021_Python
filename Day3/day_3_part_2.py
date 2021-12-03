853b47c9193b6d18cacbac12bf1851ac09af8a25
Jake <reinhartjacob827@gmail.com>
2021-12-03 17:32:41 -0500

    D3P2: refactor out split_data_by_bit_at_index

--- Day3/day_3_part_2.py
+++ Day3/day_3_part_2.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 
 def format_input(input_filename: str) -> List[str]:
@@ -7,18 +7,29 @@ def format_input(input_filename: str) -> List[str]:
         return [line.rstrip() for line in lines]
 
 
+def split_data_by_bit_at_index(data: List[str], idx: int) -> Tuple[List[str], List[str]]:
+    """
+    Splits the inputted data by the value of the bit at the given index (0s in first output, 1s in second)
+    :param data:
+    :param idx:
+    :return:
+    """
+    containing_ones = []
+    containing_zeros = []
+    for data_point in data:
+        if data_point[idx] == '0':
+            containing_zeros.append(data_point)
+        else:
+            containing_ones.append(data_point)
+    return containing_zeros, containing_ones
+
+
 def compute_oxygen_generator_rating_binary(input_filename: str):
     my_data: List[str] = format_input(input_filename)
     n = len(my_data[0])
     idx = 0
     while len(my_data) > 1:
-        containing_ones = []
-        containing_zeros = []
-        for data_point in my_data:
-            if data_point[idx] == '0':
-                containing_zeros.append(data_point)
-            else:
-                containing_ones.append(data_point)
+        (containing_zeros, containing_ones) = split_data_by_bit_at_index(my_data, idx)
         if len(containing_ones) >= len(containing_zeros):
             my_data = containing_ones
         else:
@@ -26,18 +37,13 @@ def compute_oxygen_generator_rating_binary(input_filename: str):
         idx += 1
     return my_data[0]
 
+
 def compute_co2_scrubber_rating_binary(input_filename: str):
     my_data: List[str] = format_input(input_filename)
     n = len(my_data[0])
     idx = 0
     while len(my_data) > 1:
-        containing_ones = []
-        containing_zeros = []
-        for data_point in my_data:
-            if data_point[idx] == '0':
-                containing_zeros.append(data_point)
-            else:
-                containing_ones.append(data_point)
+        (containing_zeros, containing_ones) = split_data_by_bit_at_index(my_data, idx)
         if len(containing_ones) < len(containing_zeros):
             my_data = containing_ones
         else:
@@ -46,9 +52,9 @@ def compute_co2_scrubber_rating_binary(input_filename: str):
     return my_data[0]
 
 
-
 def compute_co2_scrubber_rating(input_filename: str):
     pass
+
 
 print(compute_oxygen_generator_rating_binary('day_3_small_input.txt') == '10111')
 print(compute_co2_scrubber_rating_binary('day_3_small_input.txt') == '01010')
