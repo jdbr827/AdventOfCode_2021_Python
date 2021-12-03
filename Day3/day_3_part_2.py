390d0d09ceae08440e33b3ecdfc1e3f8dee6a7a8
Jake <reinhartjacob827@gmail.com>
2021-12-03 17:29:14 -0500

    D3P2: compute_co2_scrubber_rating_binary (passing but ugly)

--- Day3/day_3_part_2.py
+++ Day3/day_3_part_2.py
@@ -26,9 +26,29 @@ def compute_oxygen_generator_rating_binary(input_filename: str):
         idx += 1
     return my_data[0]
 
+def compute_co2_scrubber_rating_binary(input_filename: str):
+    my_data: List[str] = format_input(input_filename)
+    n = len(my_data[0])
+    idx = 0
+    while len(my_data) > 1:
+        containing_ones = []
+        containing_zeros = []
+        for data_point in my_data:
+            if data_point[idx] == '0':
+                containing_zeros.append(data_point)
+            else:
+                containing_ones.append(data_point)
+        if len(containing_ones) < len(containing_zeros):
+            my_data = containing_ones
+        else:
+            my_data = containing_zeros
+        idx += 1
+    return my_data[0]
+
 
 
 def compute_co2_scrubber_rating(input_filename: str):
     pass
 
 print(compute_oxygen_generator_rating_binary('day_3_small_input.txt') == '10111')
+print(compute_co2_scrubber_rating_binary('day_3_small_input.txt') == '01010')
