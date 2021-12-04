803c231a47ca376531d13f8e0c091dcb4755f625
Jake <reinhartjacob827@gmail.com>
2021-12-04 16:16:44 -0500

    D4P2: correct answer

--- Day4/day_4_part_1.py
+++ Day4/day_4_part_1.py
@@ -3,7 +3,7 @@ def play_bingo(filename):
     (numbers_called, boards) = read_in_boards(filename)
     found = []
     for b in range(len(boards)):
-        found.append([[0 for j in range(5)] for i in range(5)])
+        found.append([[False for _ in range(5)] for _ in range(5)])
     for number in numbers_called:
         for b in range(len(boards)):
             number_board = boards[b]
@@ -11,13 +11,37 @@ def play_bingo(filename):
             for i in range(5):
                 for j in range(5):
                     if number_board[i][j] == number:
-                        found_row = found_board[i]
-                        found_val = found_row[j]
-                        found_board[i][j] = 1
-                        if all([found_board[i][k] == 1 for k in range(5)]) or all([found_board[k][j] == 1 for k in range(5)]):
+                        found_board[i][j] = True
+                        if all([found_board[i][k] for k in range(5)]) or all([found_board[k][j] for k in range(5)]):
                             return number * sum([number_board[k][l] for k in range(5) for l in range(5) if not found_board[k][l]])
 
 
+def play_bingo_to_lose(filename):
+    (numbers_called, boards) = read_in_boards(filename)
+    found = []
+    for b in range(len(boards)):
+        found.append([[0 for j in range(5)] for i in range(5)])
+    won = [-1 for _ in range(len(boards))]
+    win_idx = 0
+    numbers_idx = 0
+    while -1 in won:
+        number = numbers_called[numbers_idx]
+        numbers_idx += 1
+        for b in range(len(boards)):
+            if won[b] == -1:
+                number_board = boards[b]
+                found_board = found[b]
+                for i in range(5):
+                    for j in range(5):
+                        if number_board[i][j] == number:
+                            found_board[i][j] = 1
+                            if all([found_board[i][k] == 1 for k in range(5)]) or all(
+                                    [found_board[k][j] == 1 for k in range(5)]):
+                                won[b] = win_idx
+                                win_idx += 1
+    winning_idx = won.index(win_idx - 1)
+    winning_board = boards[won.index(win_idx - 1)]
+    return number * sum([winning_board[k][l] for k in range(5) for l in range(5) if not found[winning_idx][k][l]])
 
 
 def read_in_boards(filename):
@@ -36,3 +60,5 @@ def read_in_boards(filename):
 
 print(play_bingo('day_4_small_input.txt') == 4512)
 print(play_bingo('day_4_input.txt') == 41503)
+print(play_bingo_to_lose('day_4_small_input.txt') == 1924)
+print(play_bingo_to_lose('day_4_input.txt'))
