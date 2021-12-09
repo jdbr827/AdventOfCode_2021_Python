c97f418bc4d12bcf6e6f42bfd47c7afc130cb3ba
Jake <reinhartjacob827@gmail.com>
2021-12-09 12:43:44 -0500

    D9P1: correct answer

---
+++ Day9/day_9.py
@@ -0,0 +1,43 @@
+from typing import List
+
+
+def read_in_matrix(filename) -> List[List[int]]:
+    matrix = []
+    with open(filename) as f:
+        while line := f.readline():
+            matrix.append(list(map(int, list(line.strip()))))
+    return matrix
+
+a = read_in_matrix('day_9_small_input.txt')
+
+def pad_matrix_with_infinitys(matrix : List[List[float]]) -> List[List[float]]:
+    n = len(matrix)
+    m = len(matrix[0])
+    for i in range(n):
+        matrix[i].insert(0, float("inf"))
+        matrix[i].append(float("inf"))
+    matrix.insert(0, [float("inf") for _ in range(m + 2)])
+    matrix.append([float("inf") for _ in range(m + 2)])
+    return matrix
+
+b = pad_matrix_with_infinitys(a)
+
+def find_low_points(padded_matrix: List[List[float]]) -> List[float]:
+    n = len(padded_matrix) - 2
+    m = len(padded_matrix[0]) - 2
+    low_points = []
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            if padded_matrix[i][j] < min(padded_matrix[i+1][j], padded_matrix[i-1][j], padded_matrix[i][j-1], padded_matrix[i][j+1]):
+                low_points.append(padded_matrix[i][j])
+    return low_points
+
+def risk_level(filename) -> int:
+    matrix = pad_matrix_with_infinitys(read_in_matrix(filename))
+    low_points = find_low_points(matrix)
+    return sum([p+1 for p in low_points])
+
+#print(risk_level('day_9_small_input.txt') == 15)
+print(risk_level('day_9_input.txt'))
+
+
