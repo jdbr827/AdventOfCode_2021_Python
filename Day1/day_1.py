9a2867fbccdca37f46ce616e9dabdc69c678d070
Jake <reinhartjacob827@gmail.com>
2021-12-10 12:43:59 -0500

    D1: some cleanup

--- Day1/day_1.py
+++ Day1/day_1.py
@@ -9,14 +9,9 @@ def count_increases(a: List[int]) -> int:
     :param a:
     :return:
     """
-    if a == []: return 0
-
-    count = 0
-    for i in range(len(a) - 1):
-        if a[i + 1] > a[i]:
-            count += 1
-    return count
+    return len([i for i in range(len(a) - 1) if a[i + 1] > a[i]])
 
 
+print(count_increases([]) == 0)
 print(count_increases([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 7)
-print(count_increases(format_input('day_1_input.txt'))) # == 1298
+print(count_increases(format_input('day_1_input.txt')) == 1298)
--- Day1/day_1_part_2.py
+++ Day1/day_1_part_2.py
@@ -12,13 +12,7 @@ def count_increases_sliding_window_3(a: List[int]):
     :return:
     """
     # A[i] + A[i+1] + A[i+2] < A[i+1] + A[i+2] + A[i+3] <==> A[i] < A[i+3]
-    if len(a) < 3:
-        return 0
-    count = 0
-    for i in range(len(a) - 3):
-        if a[i + 3] > a[i]:
-            count += 1
-    return count
+    return len([i for i in range(len(a) - 3) if a[i + 3] > a[i]])
 
 
 print(count_increases_sliding_window_3([199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5)
