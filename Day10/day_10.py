961a652ba6265f088c82ab7d031cb5349fcb83be
Jake <reinhartjacob827@gmail.com>
2021-12-10 11:50:01 -0500

    D10P1: Added type hinting for NavigationLine

--- Day10/day_10.py
+++ Day10/day_10.py
@@ -1,13 +1,17 @@
-from typing import Optional, Generator
+from typing import Optional, Generator, Literal, Union, Sequence
+
+OpenChar = Literal['(', '[', '{', '<']
+CloseChar = Literal[')', ']', '}', '>']
+NavigationLine = Sequence[Union[OpenChar, CloseChar]]
 
 
-def read_in_lines(filename: str) -> Generator[str, None, None]:
+def read_in_lines(filename: str) -> Generator[NavigationLine, None, None]:
     with open(filename) as f:
         while line := f.readline():
             yield line.strip()
 
 
-def is_line_corrupted(line: str) -> Optional[str]:
+def is_line_corrupted(line: NavigationLine) -> Optional[str]:
     """
     Determines if the inputted line is corrupted.
     If it is, returns the first illegal character in the line.
@@ -18,6 +22,6 @@ def is_line_corrupted(line: str) -> Optional[str]:
     return None
 
 
-line_generator: Generator[str, None, None] = read_in_lines('day_10_small_input.txt')
+line_generator: Generator[NavigationLine, None, None] = read_in_lines('day_10_small_input.txt')
 for line in line_generator:
     print(line)
