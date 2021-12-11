96668fc03fd378fc263af112067070fee6a1ad56
Jake <reinhartjacob827@gmail.com>
2021-12-11 14:45:20 -0500

    D11: moving to more OO structure

--- Day11/day_11.py
+++ Day11/day_11.py
@@ -1,67 +1,72 @@
-import math
+from typing import List
 
 from util import pad_matrix_with_value
 
+PAD_NUMBER: int = 100
 
-def read_in_matrix(filename):
+
+def read_in_matrix(filename) -> List[List[int]]:
     matrix = []
     with open(filename) as f:
         while line := f.readline():
-            matrix.append(list(map(float, list(line.strip()))))
+            matrix.append(list(map(int, list(line.strip()))))
     return matrix
 
 
 print(read_in_matrix('day_11_test_input_1.txt'))
 
 
-def execute_step(matrix):
-    # increase energy level of all octopi by 1
-    n = len(matrix) - 2
-    m = len(matrix[0]) - 2
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            matrix[i][j] += 1
+class OctopusMatrix:
 
-    has_flashed = [[False for _ in range(m + 2)] for _ in range(n + 2)]
+    def __init__(self, matrix: List[List[int]]):
+        self.n = len(matrix)
+        self.m = len(matrix[0])
+        self.padded_matrix = pad_matrix_with_value(matrix, PAD_NUMBER)
 
-    flashes = 0
-    flashed_this_pass = True
-    while flashed_this_pass:
-        flashed_this_pass = False
-        for i in range(1, n + 1):
-            for j in range(1, m + 1):
-                if matrix[i][j] > 9 and not has_flashed[i][j]:
-                    ## Flash
-                    flashes += 1
-                    flashed_this_pass = True
-                    has_flashed[i][j] = True
-                    for dx in [-1, 0, 1]:
-                        for dy in [-1, 0, 1]:
-                            matrix[i + dx][j + dy] += 1
+    def execute_step(self):
+        # increase energy level of all octopi by 1
+        for i in range(1, self.n + 1):
+            for j in range(1, self.m + 1):
+                self.padded_matrix[i][j] += 1
 
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if has_flashed[i][j]:
-                matrix[i][j] = 0
+        has_flashed = [[False for _ in range(self.m + 2)] for _ in range(self.n + 2)]
 
-    return matrix, flashes
+        flashes = 0
+        flashed_this_pass = True
+        while flashed_this_pass:
+            flashed_this_pass = False
+            for i in range(1, self.n + 1):
+                for j in range(1, self.m + 1):
+                    if self.padded_matrix[i][j] > 9 and not has_flashed[i][j]:
+                        ## Flash
+                        flashes += 1
+                        flashed_this_pass = True
+                        has_flashed[i][j] = True
+                        for dx in [-1, 0, 1]:
+                            for dy in [-1, 0, 1]:
+                                self.padded_matrix[i + dx][j + dy] += 1
+
+        for i in range(1, self.n + 1):
+            for j in range(1, self.m + 1):
+                if has_flashed[i][j]:
+                    self.padded_matrix[i][j] = 0
+
+        return flashes
+
 
 
 def count_flashes_over_n_steps(filename, steps):
+    octopuses: OctopusMatrix = OctopusMatrix(read_in_matrix(filename))
     flashes = 0
-    matrix = pad_matrix_with_value(read_in_matrix(filename), -math.inf)
     for _ in range(steps):
-        (matrix, flashes_this_step) = execute_step(matrix)
-        flashes += flashes_this_step
+        flashes += octopuses.execute_step()
     return flashes
 
 
 def determine_when_all_octopi_flash(filename):
-    matrix = pad_matrix_with_value(read_in_matrix(filename), -math.inf)
-    flashes_this_step = 0
-    steps = 0
-    while flashes_this_step != 100:
-        (matrix, flashes_this_step) = execute_step(matrix)
+    octopuses: OctopusMatrix = OctopusMatrix(read_in_matrix(filename))
+    steps = 1
+    while (flashes_this_step := octopuses.execute_step()) != 100:
         steps += 1
     return steps
 
