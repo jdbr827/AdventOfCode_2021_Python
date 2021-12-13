7cf15cbe776f54ed35601355fabe970fb7f3f458
Jake <reinhartjacob827@gmail.com>
2021-12-12 20:47:11 -0500

    D12: recursive solution with small case answer

--- Day12/day_12.py
+++ Day12/day_12.py
@@ -1,9 +1,12 @@
 from collections import defaultdict
 from typing import Dict, List
 
+Node = str
+Adjacency_List = Dict[Node, List[Node]]
 
-def read_in_edges(filename):
-    adjacency_graph: Dict[str, List[str]] = defaultdict(list)
+
+def read_in_edges(filename) -> Adjacency_List:
+    adjacency_graph: Adjacency_List = defaultdict(list)
     with open(filename) as f:
         while line := f.readline():
             (v1, v2) = line.strip().split("-")
@@ -11,7 +14,15 @@ def read_in_edges(filename):
             adjacency_graph[v2].append(v1)
     return adjacency_graph
 
-print(read_in_edges('day_12_small_input.txt'))
 
-def count_cave_paths_recursive(start_node, adjacency_list, visited_nodes):
-    pass
+def count_cave_paths_recursive(start_node, graph: Adjacency_List, visited_nodes: List[Node]):
+    if start_node == 'end': return 1
+    if start_node in visited_nodes: return 0
+    new_visited_nodes = visited_nodes[:]
+    if start_node.islower(): new_visited_nodes.append(start_node)
+    return sum([count_cave_paths_recursive(neighbor, graph, new_visited_nodes) for neighbor in graph[start_node]])
+
+
+g = read_in_edges('day_12_small_input.txt')
+print(g)
+print(count_cave_paths_recursive('start', g, []))
