a54d80458b54d5b1153c21134fdcb546c8993ccc
Jake <reinhartjacob827@gmail.com>
2021-12-12 20:57:58 -0500

    D12P2: correct answer

--- Day12/day_12.py
+++ Day12/day_12.py
@@ -28,7 +28,33 @@ def count_cave_paths_from_filename(filename):
     return count_cave_paths_recursive('start', g, [])
 
 
+def count_cave_paths_part_2_recursive(start_node, graph: Adjacency_List, visited_nodes: List[Node],
+                                      visited_node_twice: bool):
+    if start_node == 'end': return 1
+    if start_node in visited_nodes:
+        if start_node != 'start' and not visited_node_twice:
+            visited_node_twice = True
+        else:
+            return 0
+    new_visited_nodes = visited_nodes[:]
+    if start_node.islower(): new_visited_nodes.append(start_node)
+    return sum(
+        [count_cave_paths_part_2_recursive(neighbor, graph, new_visited_nodes, visited_node_twice) for neighbor in
+         graph[start_node]])
+
+
+def count_cave_paths_from_filename_part_2(filename):
+    g = read_in_edges(filename)
+    return count_cave_paths_part_2_recursive('start', g, [], False)
+
+
 print(count_cave_paths_from_filename('day_12_small_input.txt') == 10)
 print(count_cave_paths_from_filename('day_12_medium_input.txt') == 19)
 print(count_cave_paths_from_filename('day_12_large_input.txt') == 226)
-print(count_cave_paths_from_filename('day_12_input.txt'))
+print(count_cave_paths_from_filename('day_12_input.txt') == 4970)
+
+
+print(count_cave_paths_from_filename_part_2('day_12_small_input.txt') == 36)
+print(count_cave_paths_from_filename_part_2('day_12_medium_input.txt') == 103)
+print(count_cave_paths_from_filename_part_2('day_12_large_input.txt') == 3509)
+print(count_cave_paths_from_filename_part_2('day_12_input.txt') == 137948)
